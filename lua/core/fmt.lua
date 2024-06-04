81dd0299ea2c04787b43ffe0cac7d23947c4483b
Narendra Patwardhan <narendra@machinelearning.one>
2024-06-04 19:54:48 +0000

    Go support

--- lua/core/fmt.lua
+++ lua/core/fmt.lua
@@ -25,6 +25,7 @@ local M = { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
+      golang = { 'gofmt', 'goimports' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
