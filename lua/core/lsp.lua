603419ce3d9ab3237b7baf13986c29d24d5195fa
Narendra Patwardhan <narendra@machinelearning.one>
2024-06-12 08:08:25 +0000

    PyLSP change

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -168,7 +168,7 @@ function M.config()
       filetypes = 'rust',
       root_dir = util.root_pattern 'Cargo.toml',
     },
-    basedpyright = {},
+    pyright = {},
     -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
